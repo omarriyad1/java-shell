d51ff4e919018f97bb094a19cac2a35b9972250f
omarriad1 <riadjr74@gmail.com>
2025-09-03 15:24:41 +0300

    pass 2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        String invalidMsg=": command not found";
         //Uncomment this block to pass the first stage
          System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        System.out.println(input+invalidMsg);
     }
 }
